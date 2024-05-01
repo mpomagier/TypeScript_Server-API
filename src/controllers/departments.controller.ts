6163dac1cbf013ade034d5344e24f907b5022671
Mateusz Pomagier <mpomagier@gmail.com>
2024-05-01 17:26:50 +0200

    Adjustment index.ts & departsment.controller.ts for TypeScript

--- src/controllers/departments.controller.ts
+++ src/controllers/departments.controller.ts
@@ -2,44 +2,44 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 import { Department } from "@prisma/client";
 
-export const getAllDepartments = async (req, res) => {
+const getAllDepartments = async (req, res) => {
   return res.json(await db.department.findMany());
 };
 
-export const getDepartmentById = async (req, res) => {
+const getDepartmentById = async (req, res) => {
   const { id } = req.params;
   const department = await db.department.findUnique({ where: { id } });
-  
-  if(!department)
+
+  if (!department)
     return res.status(404).json({ message: "Department not found" });
 
   return res.json(department);
-}
+};
 
-export const createDepartment = async (req, res) => {
+const createDepartment = async (req, res) => {
   const { name, description } = req.body;
 
-  if(!name || !description)
+  if (!name || !description)
     return res.status(400).json({ message: "Invalid department data" });
 
-  const departmentData = { 
+  const departmentData = {
     name,
-    description
+    description,
   };
 
   return res.json(await db.department.create({ data: departmentData }));
-}
+};
 
-export const updateDepartment = async (req, res) => {
+const updateDepartment = async (req, res) => {
   const { id } = req.params;
   const { name, description } = req.body;
-  
-  if(!name || !description)
+
+  if (!name || !description)
     return res.status(400).json({ message: "Invalid department data" });
 
-  const departmentData = { 
+  const departmentData = {
     name,
-    description
+    description,
   };
 
   const department = await db.department.update({
@@ -47,20 +47,27 @@ export const updateDepartment = async (req, res) => {
     data: departmentData,
   });
 
-  if(!department)
+  if (!department)
     return res.status(404).json({ message: "Department not found" });
 
   return res.json(department);
-}
+};
 
-export const deleteDepartment = async (req, res) => {
+const deleteDepartment = async (req, res) => {
   const { id } = req.params;
 
   const department = await db.department.delete({ where: { id } });
 
-  if(!department)
+  if (!department)
     return res.status(404).json({ message: "Department not found" });
 
   return res.json(department);
-}
+};
 
+export {
+  getAllDepartments,
+  getDepartmentById,
+  createDepartment,
+  updateDepartment,
+  deleteDepartment,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,43 @@
-import express from 'express'
-import cors from 'cors'
-import departmentsRoutes from './routes/departments.routes'
-import employeesRoutes from './routes/employees.routes'
-import productsRoutes from './routes/products.routes'
+import express, { Application, Router } from "express";
+import cors from "cors";
+import departmentsRoutes from "./routes/departments.routes";
+import employeesRoutes from "./routes/employees.routes";
+import productsRoutes from "./routes/products.routes";
 
 class App {
-  app: express.Application
-  routes: [] = []
-  server: any
+  private app: express.Application;
+  private routes: { path: string; routes: Router }[] = [];
+  private server: any;
 
   constructor() {
-    this.app = express()
+    this.app = express();
   }
 
-  addRoutes(path, routes) {
-    this.routes.push({ path, routes })
+  public addRoutes(path: string, routes: Router): void {
+    this.routes.push({ path, routes });
   }
 
-  prepareRoutes() {
-    for(const group of this.routes) {
-      this.app.use(group.path, group.routes)
+  public prepareRoutes(): void {
+    for (const group of this.routes) {
+      this.app.use(group.path, group.routes);
     }
   }
 
-  run(port) {
+  public run(port: number | string): void {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
 
-    this.app.use(cors())
-    this.app.use(express.json())
-    this.app.use(express.urlencoded({ extended: false }))
-
-    this.prepareRoutes()
+    this.prepareRoutes();
 
     this.server = this.app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`)
-    })
+      console.log(`Server is listening on port ${port}`);
+    });
   }
-
 }
 
-const app = new App()
-app.addRoutes('/api/', departmentsRoutes)
-app.addRoutes('/api/', employeesRoutes)
-app.addRoutes('/api/', productsRoutes)
-app.run('3000')
-
+const app = new App();
+app.addRoutes("/api/", departmentsRoutes);
+app.addRoutes("/api/", employeesRoutes);
+app.addRoutes("/api/", productsRoutes);
+app.run("3000");
